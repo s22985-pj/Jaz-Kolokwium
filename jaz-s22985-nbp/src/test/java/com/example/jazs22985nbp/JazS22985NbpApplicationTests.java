201c33626c42c083a0964986584f3a272a977fa2
s22985-pj <s22985@pjwstk.edu.pl>
2022-07-03 16:45:18 +0200

    1 step

---
+++ jaz-s22985-nbp/src/test/java/com/example/jazs22985nbp/JazS22985NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs22985nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS22985NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
